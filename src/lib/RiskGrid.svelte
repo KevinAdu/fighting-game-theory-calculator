7f01635ce5c5bc936a619b7bedca2d6d76b2307c
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2025-05-17 13:02:58 +0900

    Add risk grid component - Closes #2

---
+++ src/lib/RiskGrid.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+type GridCell = {
+    myOption: string;
+    opponentOption: string;
+    risk: number;
+};
+
+export const riskMatrix: GridCell[][] = [
+    [
+      { myOption: "Block", opponentOption: "Throw", risk: 1200 },
+    ],
+    [
+        { myOption: "Delay Throw", opponentOption: "Throw", risk: 0 },
+    ],
+  ];
+
+</script>
+
+<p>{riskMatrix[0][0].myOption}</p>
